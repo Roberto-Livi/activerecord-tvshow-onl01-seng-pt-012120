71c6e71a23a9a898cc8233e50c55d59e205c1891
Roberto Livi <robertomlivi@outlook.com>
2020-03-26 23:18:16 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   end
   
   def Show::most_popular_show
-    Show.order(rating: :desc).first
+    Show.order("rating DESC").first
   end
   
   def Show::lowest_rating
