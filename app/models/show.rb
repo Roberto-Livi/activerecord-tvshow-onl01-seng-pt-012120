dab1cde17285705117a6e35ae68110ff6e18f689
Roberto Livi <robertomlivi@outlook.com>
2020-03-26 23:27:49 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -24,7 +24,7 @@ class Show < ActiveRecord::Base
   end
   
   def Show::shows_by_alphabetical_order
-    Show.order(:name).where("rating > 5 ORDER BY name")
+    Show.order(:name).where("rating > 5")
   end
 
 end
