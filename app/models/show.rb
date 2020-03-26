c12e35cddf4d8d5c6d11e0d4c02a04944538cc2b
Roberto Livi <robertomlivi@outlook.com>
2020-03-26 23:12:51 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,27 @@
 class Show < ActiveRecord::Base
-  def Show::highestr
+  def Show::highest_rating
+    
+  end
+  
+  def Show::most_popular_show
+  end
+  
+  def Show::lowest_rating
+  end
+  
+  def Show::least_popular_show
+  end
+  
+  def Show::ratings_sum
+  end
+  
+  def Show::ratings_sum
+  end
+  
+  def Show::popular_shows
+  end
+  
+  def Show::shows_by_alphabetical_order
+  end
+
 end
